758d3ea94c6b89d1c521e108e03df1a560d5b36a
unknown <ir.g.rowe@gmail.com>
2023-02-23 18:39:52 -0800

    Initial Commit- already added Basics of Curiosity, and Skim Collection. Beginning work on invitation and associated mechanics

---
+++ src/main/java/TheLibrarian/cards/Curiosity.java
@@ -0,0 +1,77 @@
+package TheLibrarian.cards;
+
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import TheLibrarian.TheLibrarianMod;
+import TheLibrarian.characters.TheLibrarian;
+
+import static TheLibrarian.TheLibrarianMod.makeCardPath;
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
+
+public class Curiosity extends AbstractDynamicCard {
+
+    /*
+     *
+     *
+     * Discard all cards, draw cards equal to anthology.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheLibrarianMod.makeID(Curiosity.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheLibrarian.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int UPGRADE_NEW_COST = 0;
+
+
+    // /STAT DECLARATION/
+
+
+    public Curiosity() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+
+
+        //this.tags.add(CardTags.STARTER_DEFEND); //Tag your strike, defend and form (Wraith form, Demon form, Echo form, etc.) cards so that they function correctly.
+    }
+
+    public Curiosity(String id, String img, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
+        super(id, img, cost, type, color, rarity, target);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int cardsToDraw = player.masterDeck.size()/10;
+        int theSize = AbstractDungeon.player.hand.size();
+        AbstractDungeon.actionManager.addToBottom(new DiscardAction((AbstractCreature) AbstractDungeon.player, (AbstractCreature) AbstractDungeon.player, theSize, false));
+        AbstractDungeon.actionManager.addToBottom(new DrawCardAction(cardsToDraw));
+
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_NEW_COST);
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/TheLibrarian/cards/SkimCollection.java
@@ -0,0 +1,84 @@
+package TheLibrarian.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.BetterDrawPileToHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import TheLibrarian.TheLibrarianMod;
+import TheLibrarian.characters.TheLibrarian;
+
+import static TheLibrarian.TheLibrarianMod.makeCardPath;
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
+import static java.lang.Math.max;
+
+public class SkimCollection extends AbstractDynamicCard {
+
+    /*
+     *
+     *
+     * Seek but doesn't exhaust and costs 5
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheLibrarianMod.makeID(SkimCollection.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheLibrarian.Enums.COLOR_GRAY;
+
+    private static final int COST = 5;
+
+
+
+    // /STAT DECLARATION/
+
+
+    public void applyPowers() {
+        this.costForTurn=max((COST-player.masterDeck.size()/10),0);
+        super.applyPowers();
+    }
+
+    public SkimCollection() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+
+        //this.tags.add(CardTags.STARTER_DEFEND); //Tag your strike, defend and form (Wraith form, Demon form, Echo form, etc.) cards so that they function correctly.
+    }
+
+    public SkimCollection(String id, String img, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
+        super(id, img, cost, type, color, rarity, target);
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+        AbstractDungeon.actionManager.addToBottom((AbstractGameAction)new BetterDrawPileToHandAction(this.magicNumber));
+
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+
+            initializeDescription();
+        }
+    }
+}
