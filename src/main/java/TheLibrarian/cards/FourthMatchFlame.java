4dfd18081991911b9b6360bc96b879552baa2dd6
unknown <ir.g.rowe@gmail.com>
2023-04-02 21:11:40 -0700

    Added Fourth Match Flame Mechanics

--- src/main/java/TheLibrarian/cards/FourthMatchFlame.java
+++ src/main/java/TheLibrarian/cards/FourthMatchFlame.java
@@ -5,6 +5,7 @@ import TheLibrarian.characters.TheLibrarian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -16,9 +17,7 @@ import static TheLibrarian.TheLibrarianMod.makeCardPath;
 public class FourthMatchFlame extends AbstractDynamicCard {
 
     /*
-     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
-     *
-     * Big Slap Deal 10(15)) damage.
+     * Deal 8(12) damage and shuffle a burn and a copy of this card into your draw pile, Repeat for every burn in your draw pile. Exhaust.
      */
 
     // TEXT DECLARATION
@@ -37,8 +36,8 @@ public class FourthMatchFlame extends AbstractDynamicCard {
     public static final CardColor COLOR = TheLibrarian.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 12;
-    private static final int UPGRADE_PLUS_DMG = 5;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_PLUS_DMG = 4;
 
     // /STAT DECLARATION/
 
@@ -46,19 +45,26 @@ public class FourthMatchFlame extends AbstractDynamicCard {
     public FourthMatchFlame() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
+        exhaust=true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-
-
-        AbstractDungeon.actionManager.addToBottom(
-                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
-                        AbstractGameAction.AttackEffect.FIRE));
-        AbstractDungeon.actionManager.addToBottom((AbstractGameAction)new MakeTempCardInDrawPileAction(new Burn(),
-                1, true, true));
-
+        int howManyBurns = 0;
+        for(AbstractCard c : p.drawPile.group) {
+            if (c.cardID=="Burn")
+                howManyBurns++;
+        }
+        for (int x=0; x<=howManyBurns; x++) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
+                            AbstractGameAction.AttackEffect.FIRE));
+            AbstractDungeon.actionManager.addToBottom((AbstractGameAction) new MakeTempCardInDrawPileAction(new Burn(),
+                    1, true, true));
+            AbstractDungeon.actionManager.addToBottom((AbstractGameAction) new MakeTempCardInDrawPileAction(new FourthMatchFlame(),
+                    1, true, true));
+        }
     }
 
     //Upgraded stats.
